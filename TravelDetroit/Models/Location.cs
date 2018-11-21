93208bf4c9e38f6dcf3ac646f9ed532076c9b257
Dominic Bierenga <dominic.bierenga@gmail.com>
2018-11-21 12:52:39 -0500

    Added User and Location models

---
+++ TravelDetroit/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelDetroit.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Url { get; set; }
+        public Dictionary<int, string> FavoritedBy { get; set; }
+    }
+}
---
+++ TravelDetroit/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelDetroit.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public Dictionary<int, string> FavoriteLocations { get; set; }
+    }
+}
