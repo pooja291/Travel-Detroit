c9debd2935edf13c53490c3248c77508aaeccd01
Dominic Bierenga <dominic.bierenga@gmail.com>
2018-11-23 19:59:21 -0500

    Added user verification and database connection

---
+++ TravelDetroit/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace TravelDetroit.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TravelDetroit.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "TravelDetroit.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(TravelDetroit.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
